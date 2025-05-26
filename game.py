57ee1598cdd2b71a1b90b64524cf81590a850c4a
Timothée Riviere <timotheeriviere10@gmail.com>
2025-05-26 17:57:24 +0200

    Added the verification of  play

--- game.py
+++ game.py
@@ -150,85 +150,86 @@ class Chess:
 
 
                 elif self.first_selection is not None and self.board[i][j] is not None and self.board[i][j].color != self.tour_actuelle or self.board[i][j] is None and self.first_selection is not None and self.first_selection != (i,j):
-                    #Deuxieme selection
+                    if button.action() in self.possible_move: 
+                        #Deuxieme selection
 
-                    if self.possible_move != []:  #Remet les couleurs d'origine
-                        for move in self.possible_move:
-                            self.buttons[move[0]*8+move[1]].color = "#769656" if (move[0]+move[1])%2 == 1 else "#EEEED2"
-                    
-                    self.list_actions.append(((self.first_selection[0],self.first_selection[1]),(i,j))) #ajout de l'action dans la liste des coups
+                        if self.possible_move != []:  #Remet les couleurs d'origine
+                            for move in self.possible_move:
+                                self.buttons[move[0]*8+move[1]].color = "#769656" if (move[0]+move[1])%2 == 1 else "#EEEED2"
+                        
+                        self.list_actions.append(((self.first_selection[0],self.first_selection[1]),(i,j))) #ajout de l'action dans la liste des coups
 
-                    self.board[self.first_selection[0]][self.first_selection[1]], self.board[i][j] = None, self.board[self.first_selection[0]][self.first_selection[1]]
-                    self.board[i][j].rect.x = self.buttons[i*8+j].rect.x #deplacement de la piece
-                    self.board[i][j].rect.y = self.buttons[i*8+j].rect.y #deplacement de la piece
-                    #Joue le coup
+                        self.board[self.first_selection[0]][self.first_selection[1]], self.board[i][j] = None, self.board[self.first_selection[0]][self.first_selection[1]]
+                        self.board[i][j].rect.x = self.buttons[i*8+j].rect.x #deplacement de la piece
+                        self.board[i][j].rect.y = self.buttons[i*8+j].rect.y #deplacement de la piece
+                        #Joue le coup
 
-                    if isinstance(self.board[i][j],(king,rook)):
-                        if self.board[i][j].has_rocked == False:
-                            self.board[i][j].has_rocked = True #On verifie si la piece a bougé
+                        if isinstance(self.board[i][j],(king,rook)):
+                            if self.board[i][j].has_rocked == False:
+                                self.board[i][j].has_rocked = True #On verifie si la piece a bougé
 
-                    if isinstance(self.board[i][j],king):
-                        if self.board[i][j].color == "White":
-                            self.pos_white_king = [i,j]
-                        else:
-                            self.pos_black_king = [i,j]
-                        if abs(self.first_selection[1]-j) == 2: #Roque
-                            if self.first_selection[1] < j: #Roque à droite
-                                self.board[i][7],self.board[i][5] = None, self.board[i][7]
-                                self.board[i][5].rect.x = self.buttons[i*8+5].rect.x #deplacement de la piece
-                                self.board[i][5].rect.y = self.buttons[i*8+5].rect.y #deplacement de la piece
-                            else: #Roque à gauche
-                                self.board[i][0],self.board[i][3] = None, self.board[i][0]
-                                self.board[i][3].rect.x = self.buttons[i*8+3].rect.x #deplacement de la piece
-                                self.board[i][3].rect.y = self.buttons[i*8+3].rect.y#deplacement de la piece
-
-                    self.tour_actuelle = "Black" if self.tour_actuelle == "White" else "White" #change le tour
-
-
-                    self.buttons[self.first_selection[0]*8+self.first_selection[1]].color = "#ffa500" ##Change les couleurs des cases qui ont été jouées
-                    self.buttons[i*8+j].color = "#a9ff9f"
-
-
-                    if len(self.list_actions) > 1:
-                        self.buttons[self.list_actions[-2][0][0]*8+self.list_actions[-2][0][1]].color = "#769656" if (self.list_actions[-2][0][0]+self.list_actions[-2][0][1])%2 == 1 else "#EEEED2"
-                        self.buttons[self.list_actions[-2][1][0]*8+self.list_actions[-2][1][1]].color = "#769656" if (self.list_actions[-2][1][0]+self.list_actions[-2][1][1])%2 == 1 else "#EEEED2"
-                    
-                
-                    if isinstance(self.board[i][j],pawn): #cas particuliers du pion
-                        if self.pawn_jump != None: #verification du saut du pion
-                            if self.board[i][j].color == "White" and i > 0 and i + 1 == self.pawn_jump[0] and j == self.pawn_jump[1]:
-                                self.board[i+1][j] = None
-                            elif self.board[i][j].color == "Black" and i < 7 and i - 1 == self.pawn_jump[0] and j == self.pawn_jump[1]:
-                                self.board[i-1][j] = None
+                        if isinstance(self.board[i][j],king):
+                            if self.board[i][j].color == "White":
+                                self.pos_white_king = [i,j]
                             else:
-                                print(i-1,j,self.pawn_jump)
-                                print(i+1,j,self.pawn_jump)
+                                self.pos_black_king = [i,j]
+                            if abs(self.first_selection[1]-j) == 2: #Roque
+                                if self.first_selection[1] < j: #Roque à droite
+                                    self.board[i][7],self.board[i][5] = None, self.board[i][7]
+                                    self.board[i][5].rect.x = self.buttons[i*8+5].rect.x #deplacement de la piece
+                                    self.board[i][5].rect.y = self.buttons[i*8+5].rect.y #deplacement de la piece
+                                else: #Roque à gauche
+                                    self.board[i][0],self.board[i][3] = None, self.board[i][0]
+                                    self.board[i][3].rect.x = self.buttons[i*8+3].rect.x #deplacement de la piece
+                                    self.board[i][3].rect.y = self.buttons[i*8+3].rect.y#deplacement de la piece
 
-                        if self.board[i][j].has_jumped(i,self.first_selection[0]) == True: #verification du saut du pion
-                            self.pawn_jump = (i,j)
-                        else:
-                            self.pawn_jump = None
-                        if self.board[i][j].upgrade(i) == True:
-                            self.current_upgrade = (i,j)
-                            for piece in self.buttons_upgrade_board:
-                                piece.__init__("White" if self.tour_actuelle == "Black" else "Black",self.screen)
-                                i = -1
+                        self.tour_actuelle = "Black" if self.tour_actuelle == "White" else "White" #change le tour
+
+
+                        self.buttons[self.first_selection[0]*8+self.first_selection[1]].color = "#ffa500" ##Change les couleurs des cases qui ont été jouées
+                        self.buttons[i*8+j].color = "#a9ff9f"
+
+
+                        if len(self.list_actions) > 1:
+                            self.buttons[self.list_actions[-2][0][0]*8+self.list_actions[-2][0][1]].color = "#769656" if (self.list_actions[-2][0][0]+self.list_actions[-2][0][1])%2 == 1 else "#EEEED2"
+                            self.buttons[self.list_actions[-2][1][0]*8+self.list_actions[-2][1][1]].color = "#769656" if (self.list_actions[-2][1][0]+self.list_actions[-2][1][1])%2 == 1 else "#EEEED2"
+                        
+                    
+                        if isinstance(self.board[i][j],pawn): #cas particuliers du pion
+                            if self.pawn_jump != None: #verification du saut du pion
+                                if self.board[i][j].color == "White" and i > 0 and i + 1 == self.pawn_jump[0] and j == self.pawn_jump[1]:
+                                    self.board[i+1][j] = None
+                                elif self.board[i][j].color == "Black" and i < 7 and i - 1 == self.pawn_jump[0] and j == self.pawn_jump[1]:
+                                    self.board[i-1][j] = None
+                                else:
+                                    print(i-1,j,self.pawn_jump)
+                                    print(i+1,j,self.pawn_jump)
+
+                            if self.board[i][j].has_jumped(i,self.first_selection[0]) == True: #verification du saut du pion
+                                self.pawn_jump = (i,j)
+                            else:
+                                self.pawn_jump = None
+                            if self.board[i][j].upgrade(i) == True:
+                                self.current_upgrade = (i,j)
                                 for piece in self.buttons_upgrade_board:
-                                    i += 1
-                                    if piece is not None:
-                                        piece.rect.x = self.buttons_upgrade[i].rect.x
-                                        piece.rect.y = self.buttons_upgrade[i].rect.y
+                                    piece.__init__("White" if self.tour_actuelle == "Black" else "Black",self.screen)
+                                    i = -1
+                                    for piece in self.buttons_upgrade_board:
+                                        i += 1
+                                        if piece is not None:
+                                            piece.rect.x = self.buttons_upgrade[i].rect.x
+                                            piece.rect.y = self.buttons_upgrade[i].rect.y
 
-                    if self.tour_actuelle == "White": #Vérification d'echec et mat
-                        mat =self.verif_echec_mat(self.pos_white_king)
-                    else:
-                        mat = self.verif_echec_mat(self.pos_black_king)
-                    if mat:
-                        self.winner = "Black" if self.tour_actuelle == "White" else "White"
-                        print("Echec et mat")
-                    else:
-                        print("continue")
-                    self.first_selection = None
+                        if self.tour_actuelle == "White": #Vérification d'echec et mat
+                            mat =self.verif_echec_mat(self.pos_white_king)
+                        else:
+                            mat = self.verif_echec_mat(self.pos_black_king)
+                        if mat:
+                            self.winner = "Black" if self.tour_actuelle == "White" else "White"
+                            print("Echec et mat")
+                        else:
+                            print("continue")
+                        self.first_selection = None
     def verif_echec_mat(self,pos_king):
         is_attacked,attaqueur =self.board[pos_king[0]][pos_king[1]].verif_echec(self.board,(pos_king[0],pos_king[1]),verif=True)
         if is_attacked:
